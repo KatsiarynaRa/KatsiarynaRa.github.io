c780ccc96cc4ae1e897c1494e7e304bc9965ee65
KatsiarynaRa <115622769+KatsiarynaRa@users.noreply.github.com>
2023-03-31 15:46:34 +0300

    mp

---
+++ music app/data.js
@@ -0,0 +1,46 @@
+let albums = [
+    {'images/unsplash_YrtFlrLo2DQ.png'},
+    {'images/unsplash_DBGwy7s3QY0.png'},
+    {'images/unsplash_h6gTtqBZUxc.png'},
+    {'images/unsplash_ta7rN3NcWyM.png'},
+
+]
+
+let songs = [
+    {
+        cover: 'images/Katy Perry-Roar.png',
+        artist: 'Katy Perry',
+        song: 'Roar',
+        duration: '3:20',
+        path: 'musics/Roar.mp3',
+        
+        
+    },
+    {
+        cover: 'images/Taylor Swift-Snake.png',
+        artist: 'Taylor Swift',
+        song: 'Shake it Off ',
+        duration: '3:45',
+        path: 'musics/Shake it off.mp3',
+        
+        
+    },
+    {
+        cover: 'images/Marshmello&Anne-Marrie-Friends.png',
+        artist: 'Marshmello & Anne-Marie',
+        song: 'FRIENDS',
+        duration: '3:05',
+        path: 'musics/FRIENDS.mp3',
+        
+        
+    },
+    {
+        cover: 'images/Justin Bieber- Baby.png',
+        artist: 'Justin Bieber - Baby',
+        song: 'Baby',
+        duration: '3:45',
+        path: 'musics/Baby.mp3',
+        
+        
+    },
+]
