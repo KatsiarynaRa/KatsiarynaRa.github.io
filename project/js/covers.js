ea1c46261d9e767ca1593d6930609b9cca7da264
KatsiarynaRa <115622769+KatsiarynaRa@users.noreply.github.com>
2023-05-28 19:18:09 +0300

    changing

--- project/js/covers.js
+++ project/js/covers.js
@@ -1,30 +1,30 @@
 const covers = [
   {
     id: 1,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/637fa8eb-1ac5-40ee-9c37-0ba1c794d170/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/ru/2/2b/Star_Wars_Episode_I_-_The_Phantom_Menace_Game.JPG",
   },
   {
     id: 2,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/1216c8d2-9c47-49e9-b5c2-e8d5f01ca0ac/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/ru/thumb/a/ad/Star_Wars_Attack_of_the_Clones_poster.jpg/640px-Star_Wars_Attack_of_the_Clones_poster.jpg",
   },
   {
     id: 3,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/61122147-881f-43a0-9152-981e04e34332/1920x",
+    src: "https://upload.wikimedia.org/wikipedia/ru/9/91/Star_Wars_Episode_III_game_cover.png",
   },
   {
     id: 4,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/9bdc6690-de82-4a8c-a114-aa3a353bc1da/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Star_Wars_%281997_re-release_poster%29.jpg/170px-Star_Wars_%281997_re-release_poster%29.jpg",
   },
   {
     id: 5,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/42f4a53f-b353-4700-bbb7-525837c46630/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/ru/thumb/e/e0/Empire20strikes20back_old.jpg/230px-Empire20strikes20back_old.jpg",
   },
   {
     id: 6,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/1ae91486-01cd-424e-8562-119a73add1e6/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/ru/b/b2/ReturnOfTheJediPoster1983.jpg",
   },
   {
     id: 7,
-    src: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/cc8c9b26-1c84-4a47-a502-cbb02fb217df/300x450",
+    src: "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
   }
 ];
